a3ee92355631ece90b8e47232a23bb653f5a8d3f
khsacc <hero.key629522@gmail.com>
2020-09-30 09:11:40 +0900

    reset browser default styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app';
+import { Header } from '../components/header'
+import Head from 'next/head'
+import CssBaseLine from '@material-ui/core/CssBaseline'
 
 const defaultLayout = ({ Component, pageProps }: AppProps) => (
   <>
+    <CssBaseLine />
+    <Head>
+    </Head>
+    <Header />
     <Component {...pageProps} />
   </>
 )
