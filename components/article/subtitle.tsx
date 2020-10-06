d784fadf255cee62f032621c45e1079d5bce01d9
khsacc <hero.key629522@gmail.com>
2020-10-06 20:21:17 +0900

    added umbrella decoration on sub-title component

--- components/article/subtitle.tsx
+++ components/article/subtitle.tsx
@@ -1,10 +1,23 @@
+import { LineUmb } from '../common';
 import { NextPage } from 'next';
+import { colours, mrEavesRegular, zIndex } from '../../styles';
 import { makeStyles } from '@material-ui/core';
-import { mrEavesRegular, zIndex } from '../../styles';
 
 export const Subtitle: NextPage = props => {
   // 致命的な問題として、このコンポーネント日本語に使えません
   const classes = makeStyles(theme => ({
+    wrapper: {
+      position: 'relative',
+      marginTop: '1em',
+    },
+    umb: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      height: 80,
+      color: colours.main.main,
+    },
     name: {
       ...mrEavesRegular,
       textAlign: 'center',
@@ -15,7 +28,14 @@ export const Subtitle: NextPage = props => {
         fontSize: '2.5em',
       },
       fontFeatureSettings: '"palt"',
+      background: colours.main.subtitle_back,
+      margin: '1.7em 0',
     },
   }))();
-  return <h2 className={classes.name}>{props.children}</h2>;
+  return (
+    <div className={classes.wrapper}>
+      <LineUmb className={classes.umb} />
+      <h2 className={classes.name}>{props.children}</h2>
+    </div>
+  );
 };
--- styles/colours.ts
+++ styles/colours.ts
@@ -5,6 +5,7 @@ export const colours = {
     main: '#de4575',
     sub: '#c15f72',
     back: '#fcf0f0',
+    subtitle_back: 'rgba(252, 240, 240, 0.5)',
     light: '#f2cbd2',
     hover: '#fcdce0',
   },
