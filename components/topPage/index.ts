39d5f6c1e288a318e2057601cabbcf8c56df6574
khsacc <hero.key629522@gmail.com>
2020-12-27 01:26:31 +0900

    added export

--- components/topPage/index.ts
+++ components/topPage/index.ts
@@ -1,4 +1,5 @@
+import { Contact } from './contact';
 import { Self } from './self';
 import { Top } from './top';
 import { Works } from './works';
-export { Self, Top, Works };
+export { Self, Top, Works, Contact };
