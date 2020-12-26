93f541c7992ed1303eb83c878c906f6b70c40ea8
khsacc <hero.key629522@gmail.com>
2020-12-26 18:18:51 +0900

    added parallax effect

--- components/article/title.tsx
+++ components/article/title.tsx
@@ -1,5 +1,6 @@
 import { LineUmb } from '../common';
 import { NextPage } from 'next';
+import { Parallax } from 'react-scroll-parallax';
 import { colours } from '../../styles';
 import { makeStyles } from '@material-ui/core';
 import { transitionTimingfunc } from '../../styles/global';
@@ -64,7 +65,9 @@ export const ArticleTitle: NextPage<{ img: string; title: string; umbColour?: st
   }, []);
   return (
     <div className={classes.wrapper} style={{ transform: `translateX(${entered ? 0 : -4}%)` }}>
-      <img src={img} className={[classes.article__img].join(' ')} alt={alt} />
+      <Parallax y={[3, -2]}>
+        <img src={img} className={[classes.article__img].join(' ')} alt={alt} />
+      </Parallax>
       <span className={[classes.article__title, entered ? classes.article__title__shown : ''].join(' ')}>{title}</span>
       <LineUmb className={[classes.umb, entered ? classes.umb__shown : ''].join(' ')} />
     </div>
