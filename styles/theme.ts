98736b25124dd52a0991fa736d6140639c5f1cb6
khsacc <hero.key629522@gmail.com>
2020-12-28 04:10:48 +0900

    changed path

--- styles/theme.ts
+++ styles/theme.ts
@@ -20,7 +20,7 @@ const glowSansCondR = {
   fontStyle: 'sans-serif',
   // fontDisplay: 'swap',
   fontWeight: 400,
-  src: `url("https://hiroki-k.vercel.app/typefaces/GlowSansJ-Condensed-Regular.woff") format('woff')`,
+  src: `url("https://portfolio2020-2.khsacc.vercel.app/typefaces/GlowSansJ-Condensed-Regular.woff") format('woff')`,
   // unicodeRange:
   //   'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
 };
