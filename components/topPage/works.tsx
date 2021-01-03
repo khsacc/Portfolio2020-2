85a0f59668cf00f1d604d2ea41278550858f72aa
khsacc <hero.key629522@gmail.com>
2021-01-04 03:28:23 +0900

    added a work

--- components/topPage/works.tsx
+++ components/topPage/works.tsx
@@ -39,9 +39,11 @@ type ImgData = {
     bottom?: number;
   };
   dir: 'left' | 'right';
+  textAlign?: 'start' | 'end' | 'left' | 'right' | 'center' | 'justify' | 'match-parent';
   src: string;
   className: string;
   alt?: string;
+  colour?: string;
 };
 
 const ImgContainer: NextPage<{ data: ImgData[] }> = ({ data }) => {
@@ -55,6 +57,7 @@ const ImgContainer: NextPage<{ data: ImgData[] }> = ({ data }) => {
       width: 'fit-content',
       position: 'relative',
       overflow: 'hidden',
+      margin: '15px 10px 0',
       '&:hover': {
         '& $eachImgName': {
           opacity: 1,
@@ -66,7 +69,6 @@ const ImgContainer: NextPage<{ data: ImgData[] }> = ({ data }) => {
       position: 'absolute',
       opacity: 0,
       color: 'white',
-      background: colours.main.img_name,
       transition: 'all 0.5s ease-out',
       padding: '5px 0 2px',
     },
@@ -88,8 +90,20 @@ const ImgContainer: NextPage<{ data: ImgData[] }> = ({ data }) => {
           data-aos-easing="ease-out-quad"
         >
           <img src={datum.src} className={[datum.className, classes.eachImg].join(' ')} alt={datum.alt ?? ''} />
-          <span className={classes.eachImgName} style={{ ...datum.namePosition }}>
-            {datum.name}
+          <span
+            className={classes.eachImgName}
+            style={{
+              ...datum.namePosition,
+              background: datum.colour ?? colours.main.img_name,
+              textAlign: datum.textAlign ?? 'left',
+            }}
+          >
+            {datum.name.split('\n').map((e, i) => (
+              <span key={i}>
+                {e}
+                <br />
+              </span>
+            ))}
           </span>
         </div>
       ))}
@@ -125,11 +139,11 @@ export const Works: NextPage = () => {
     },
     MF92_img: {
       height: 300,
-      margin: '15px 10px 0',
+      // margin: '15px 10px 0',
     },
     MF93_web: {
       height: 400,
-      margin: '15px 10px 0',
+      // margin: '15px 10px 0',
     },
     separateUmb: {
       width: 100,
@@ -173,19 +187,33 @@ export const Works: NextPage = () => {
               },
             ]}
           />
-          {/* <div className={classes.img_container}>
-            <div style={{ display: 'inline-block', height: 'fit-content', width: 'fit-content', position: 'relative' }}>
-              <span style={{ position: 'absolute', top: 0, left: 0 }}>ポスター</span>
-              <img
-                data-aos="fade-right"
-                data-aos-duration="800"
-                data-aos-easing="ease-out-quad"
-                src="/img/works/KF70_poster.jpg"
-                className={classes.KF70_img}
-                alt=""
-              ></img>
-            </div>
-          </div> */}
+        </Parallax>
+        <Parallax y={[20, -30]} data-aos="fade-up">
+          <FesInfo
+            title="第93回五月祭"
+            categories={['Web Frontend', 'DTP Design', 'Design Workshop']}
+            info="2020年・共同制作（情報技術部・印刷物担当統括・オンライン支援検討ワーキンググループ）"
+          />
+          <ImgContainer
+            data={[
+              {
+                name: '公式ウェブサイト\nグッズページ',
+                namePosition: { left: 0, top: 0 },
+                src: '/img/works/MF93/web_goods.jpg',
+                dir: 'right',
+                className: classes.MF93_web,
+                colour: 'rgba(58, 148, 128, 0.8)',
+              },
+              {
+                name: '公式ウェブサイト\nオンライン開催ページ',
+                namePosition: { left: 0, top: 0 },
+                src: '/img/works/MF93/web_online.jpg',
+                dir: 'left',
+                className: classes.MF93_web,
+                colour: 'rgba(58, 148, 128, 0.8)',
+              },
+            ]}
+          />
         </Parallax>
         <Parallax y={[5, -40]} data-aos="fade-up">
           <FesInfo
@@ -199,42 +227,28 @@ export const Works: NextPage = () => {
             <br />
             「〈おもしろい〉が交差する」というテーマを、無機質になりがちな案内サインの中に取り入れて、ビジュアルデザイン的な面白さも併存するように工夫しています。
           </p>
-          <div className={classes.img_container}>
-            <img
-              src="/img/works/MF92navi.jpg"
-              data-aos="fade-right"
-              data-aos-duration="800"
-              data-aos-easing="ease-out-quad"
-              className={classes.MF92_img}
-              alt=""
-            ></img>
-          </div>
-        </Parallax>
-        <Parallax y={[20, -30]} data-aos="fade-up">
-          <FesInfo
-            title="第93回五月祭"
-            categories={['Web Frontend', 'DTP Design', 'Design Workshop']}
-            info="2020年・共同制作（情報技術部・印刷物担当統括・オンライン支援検討ワーキンググループ）"
+          <ImgContainer
+            data={[
+              {
+                name: '教室案内サイン',
+                namePosition: { left: 0, top: 0 },
+                src: '/img/works/MF92navi.jpg',
+                dir: 'right',
+                className: classes.MF92_img,
+                colour: 'rgba(58, 148, 128, 0.8)',
+              },
+              {
+                name: '公式パンフレット表紙',
+                namePosition: { left: 0, top: 0 },
+                src: '/img/works/MF92pam_cover.png',
+                dir: 'left',
+                className: classes.MF92_img,
+                colour: 'rgba(58, 148, 128, 0.8)',
+              },
+            ]}
           />
-          <div className={classes.img_container}>
-            <img
-              src="/img/works/MF93/web_goods.jpg"
-              data-aos="fade-right"
-              data-aos-duration="800"
-              data-aos-easing="ease-out-quad"
-              className={classes.MF93_web}
-              alt=""
-            />
-            <img
-              src="/img/works/MF93/web_online.jpg"
-              data-aos="fade-left"
-              data-aos-duration="800"
-              data-aos-easing="ease-out-quad"
-              className={classes.MF93_web}
-              alt=""
-            />
-          </div>
         </Parallax>
+
         <LineUmb className={classes.separateUmb} />
         <div className={classes.description}>other works</div>
         <div className={classes.img_container}>
