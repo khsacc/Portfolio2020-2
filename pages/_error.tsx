2a3ee7a735f4fc53a14a295a37ea77faa6bdee06
khsacc <hero.key629522@gmail.com>
2021-02-25 17:33:45 +0900

    improved error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,40 +1,11 @@
+import { ErrorPage } from '../components/common/error';
 import { NextPage, NextPageContext } from 'next';
-import { colours } from '../styles';
-import { makeStyles } from '@material-ui/core';
 import React from 'react';
 
-// https://nextjs.org/docs/advanced-features/custom-error-page
-
-const useStyles = makeStyles(() => ({
-  wrapper: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: 'calc(100vh - 100px)',
-    width: '100%',
-    fontSize: '1.5em',
-  },
-  statusCode: {
-    fontWeight: 'bold',
-    display: 'inline-block',
-    margin: 10,
-    padding: 10,
-    borderRight: `1px solid ${colours.main.dark}`,
-  },
-}));
-
-const Error: NextPage<{ statusCode: number }> = ({ statusCode }) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.wrapper}>
-      <span className={classes.statusCode}>{statusCode}</span>
-      {statusCode === 404 ? 'Page not found.' : 'error occurred.'}
-    </div>
-  );
-};
+const Error: NextPage<{ statusCode: number }> = ({ statusCode }) => <ErrorPage statusCode={statusCode} />;
 
 Error.getInitialProps = async ({ res }: NextPageContext) => ({
   statusCode: res.statusCode,
-}); // SSR
+});
 
 export default Error;
