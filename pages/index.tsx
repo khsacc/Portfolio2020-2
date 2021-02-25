e02cf795ff42b0a7550588d9a55d12f92da34f68
khsacc <hero.key629522@gmail.com>
2021-02-26 02:19:03 +0900

    fixed path

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const IndexPage: NextPage = () => {
     <>
       <CreateHead />
       <Top />
-      <Link href="profile" scroll={false}>
+      <Link href="/profile" scroll={false}>
         <a className={classes.wrapper_link}>
           <Self />
         </a>
