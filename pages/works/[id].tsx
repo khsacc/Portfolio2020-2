20cb80026c3ac944e33b14b0db7d3782f03f7c29
khsacc <hero.key629522@gmail.com>
2021-02-25 17:33:33 +0900

    allowed dynamic routing

---
+++ pages/works/[id].tsx
@@ -0,0 +1,27 @@
+import { NextPage } from 'next';
+import { PageContent } from '../../components/common/pageContent';
+import { ProjectInfo, WorksDatum, worksData } from '../../components/works';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({}));
+
+const ProjectPage: NextPage<{ prj: WorksDatum }> = ({ prj }) => {
+  const classes = useStyles();
+  return (
+    <PageContent>
+      <ProjectInfo prj={prj} />
+    </PageContent>
+  );
+};
+
+export async function getStaticPaths() {
+  return { paths: worksData.map(e => `/works/${e.id}`), fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const id = params.id;
+  const prj = worksData.find(e => e.id === id);
+  return { props: { prj } };
+}
+
+export default ProjectPage;
