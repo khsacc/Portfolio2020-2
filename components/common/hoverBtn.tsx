8704c32f7476f6dd573e98854b084f6794909991
khsacc <hero.key629522@gmail.com>
2021-02-27 19:38:51 +0900

    improved hover-button design in mobile devices

--- components/common/hoverBtn.tsx
+++ components/common/hoverBtn.tsx
@@ -3,6 +3,7 @@ import { NextPage } from 'next';
 import { colours } from '../../styles';
 import { makeStyles } from '@material-ui/core';
 import { transitionTimingfunc } from '../../styles/global';
+import UAParser from 'ua-parser-js';
 
 const useStyles = makeStyles(() => ({
   btn: {
@@ -23,15 +24,21 @@ const useStyles = makeStyles(() => ({
 }));
 
 // Wrapper
-export const HoverBtn: NextPage<{ text?: string | ReactNode; hover?: boolean; style?: CSSProperties }> = ({
-  text,
-  hover,
-  style,
-}) => {
+export const HoverBtn: NextPage<{
+  text?: string | ReactNode;
+  hover?: boolean;
+  style?: CSSProperties;
+  avoidMobile?: boolean;
+}> = ({ text, hover, style, avoidMobile = false }) => {
   const classes = useStyles();
 
+  const uaParser = new UAParser(); // todo: hydration errorが起こる可能性がある
+  const isHoverAllowed = !['mobile', 'tablet,'].includes(`${uaParser.getDevice().type}`);
+
+  const hoverParameter = !avoidMobile ? hover : isHoverAllowed ? hover : true;
+
   return (
-    <span className={[classes.btn, hover ? classes.btn_hover : ''].join(' ')} style={style}>
+    <span className={[classes.btn, hoverParameter ? classes.btn_hover : ''].join(' ')} style={style}>
       {text}
     </span>
   );
