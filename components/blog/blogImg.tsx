e1de72a93c33c58166223c0a3eabe3004b72cdd9
khsacc <hero.key629522@gmail.com>
2021-03-02 00:59:40 +0900

    utilize external link component

--- components/blog/blogImg.tsx
+++ components/blog/blogImg.tsx
@@ -1,8 +1,6 @@
-import * as gtag from '../../lib/gtag';
 import { ExternalLink } from '../common/externalLink';
 import { NextPage } from 'next';
 import { WorkImg } from '../works';
-import { useRouter } from 'next/dist/client/router';
 import React from 'react';
 
 // 執筆しやすいようにprop構造を変更しただけ
@@ -14,20 +12,11 @@ export const BlogImg: NextPage<{
   alt?: string;
   width?: string;
   href?: string;
-}> = ({ name, note, img, alt, width, href }) => {
-  const currentPathName = useRouter().pathname;
-  return typeof href === 'undefined' ? (
+}> = ({ name, note, img, alt, width, href }) =>
+  typeof href === 'undefined' ? (
     <WorkImg work={{ name, note, img, alt: alt || '', topPage: false }} imgWidth={width}></WorkImg>
   ) : (
-    <a
-      href={href}
-      rel="external nofollow noopener noreferrer"
-      target="_blank"
-      onClick={() => {
-        gtag.event({ action: 'exLink', category: 'blog', label: `from-${currentPathName}-to-${href}` });
-      }}
-    >
+    <ExternalLink href={href} category="blog-image">
       <WorkImg work={{ name, note, img, alt: alt || '', topPage: false }} imgWidth={width}></WorkImg>
-    </a>
+    </ExternalLink>
   );
-};
