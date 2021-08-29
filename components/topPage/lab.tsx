88c4e0ec6f57d890909dfb8488447ae7cdb30759
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-08-29 11:57:22 +0900

    added new lab work

--- components/topPage/lab.tsx
+++ components/topPage/lab.tsx
@@ -15,6 +15,11 @@ const laboratoryData: WorksDetail[] = [
     topPage: true,
   },
   {
+    img: '/img/works/lab/info.jpg',
+    alt: 'information',
+    topPage: true,
+  },
+  {
     img: '/img/works/lab/imperfect.jpg',
     alt: 'imperfect',
     topPage: true,
